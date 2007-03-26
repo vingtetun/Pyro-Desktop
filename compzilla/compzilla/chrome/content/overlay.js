cc03ef55649cd776523724f8c309450662fd56f1
alexgraveley <alexgraveley@bcc094ed-0e32-0410-8ff4-e4362d8942ad>
2007-03-26 02:11:53 +0000

    Install into $(GECKO_LIBDIR)/extensions/compzilla@beatniksoftware.com/ and begin the overlay code.
    git-svn-id: http://pyrodesktop.org/svn/trunk/pyrodesktop@4 bcc094ed-0e32-0410-8ff4-e4362d8942ad

--- compzilla/compzilla/chrome/content/overlay.js
+++ compzilla/compzilla/chrome/content/overlay.js
@@ -1,2 +1,14 @@
 
-// Nothing yet.
+window.hidechrome = true
+window.screenX = 0
+window.screenY = 0
+window.sizemode = maximized
+
+window.addEventListener("onload", compzillaLoad, true)
+
+function compzillaLoad()
+{
+	cls = Components.classes['@beatniksoftware.com/compzillaService;1']
+	svc = cls.getService(Components.interfaces.compzillaIControl);
+	svc.RegisterWindowManager(window)
+}
