9e3cec8b6e6426f4182fc96c6947cdc76d8344a9
toshok <toshok@bcc094ed-0e32-0410-8ff4-e4362d8942ad>
2007-05-08 13:22:34 +0000

    2007-05-08  Chris Toshok  <toshok@hungry.com>
    	* compzilla/chrome/content/windowStack.js: document this a little
    	bit, fix up some incorrect args to stackWindow, and only dangle
    	off the windowStack element those methods which are public.
    	Private ones are defined in the local file scope.
    
    git-svn-id: http://pyrodesktop.org/svn/trunk/pyrodesktop@98 bcc094ed-0e32-0410-8ff4-e4362d8942ad

--- compzilla/compzilla/chrome/content/windowStack.js
+++ compzilla/compzilla/chrome/content/windowStack.js
@@ -1,68 +1,50 @@
-function Debug (str)
-{
-  // hack.  compzillaWindowManager.js installs its Debug function on
-  // the document.body so we can get access to it here.
-  windowStack.parentNode.Debug (str);
-}
+/* -*- mode: javascript; c-basic-offset: 4; indent-tabs-mode: t; -*- */
+
+/*
+ *  we install a few public methods on the 'windowStack' element in start.xul
+ *
+ *  stackWindow (w) - this is the entry point to use when initially
+ *                    adding a window to the stack.
+ *
+ *  replaceWindow (w1, w2) - replaces window @w1 with @w2.  if @w1 is
+ *                           not presently in the stack, this has the
+ *                           same behavior as stackWindow(@w2).
+ *
+ *  removeWindow (w) - removes @w from the window stack.
+ *
+ *  moveAbove (w, above) - moves @w above @above in the stacking order.
+ *                         If they're in 2 different layers, nothing
+ *                         happens.  If @w or @above are not presently
+ *                         in the window stack, they are added before
+ *                         the moveAbove operation is performed.
+ *
+ *  moveToBottom (w) - moves @w to the bottom of its layer.  If @w is
+ *                     not in the window stack, no action is taken.
+ *
+ *  moveToTop (w) - moves @w to the top of its layer.  If @w is not in
+ *                  the window stack, no action is taken.
+ *
+ *  toggleDesktop () - toggles all layers above the desktop-layer.
+ */
+
+// HACK:
+// compzillaWindowManager.js installs two things on it's body tag (id = "desktop"):
+//
+// 1. the Debug function that allows us to spew to the debug window
+//    from here
+// 2. the Atoms object which allows us to share the interned js cache.
+//
+// These are accessed from here by "windowStack.parentNode.Debug" and
+// "windowStack.parentNode.Atoms".
 
 var windowStack = document.getElementById ('windowStack');
 
-windowStack.showingDesktop = false;
-windowStack.layers = new Array ();
-
-for (var el = windowStack.firstChild; el != null; el = el.nextSibling) {
-  if (el.id == "desktop-layer")         windowStack.layers[0] = el;
-  else if (el.id == "below-layer")      windowStack.layers[1] = el;
-  else if (el.id == "normal-layer")     windowStack.layers[2] = el;
-  else if (el.id == "dock-layer")       windowStack.layers[3] = el;
-  else if (el.id == "fullscreen-layer") windowStack.layers[4] = el;
-  else if (el.id == "expose-layer")     windowStack.layers[5] = el;
-  else if (el.id == "picker-layer")     windowStack.layers[6] = el;
-  else if (el.id == "overlay-layer")    windowStack.layers[7] = el;
-  else if (el.id == "debug-layer")      windowStack.layers[8] = el;
-}
-
-
-// add a resize event handler on the parentNode so we can resize the window stack
-windowStack.parentNode.onresize = function (e) {
-  windowStack.updateSize ();
-}
-
-windowStack.updateSize = function () {
-  /* make sure the window stack takes up 100% of the size available to it */
-  windowStack.style.width = windowStack.parentNode.clientWidth;
-  windowStack.style.height = windowStack.parentNode.clientHeight;
-}
-
-windowStack.determineLayer = function (w) {
-  /* from EWMH:
-     from bottom to top:
-     * 0: windows of type _NET_WM_WINDOW_TYPE_DESKTOP
-     * 1: windows having state _NET_WM_STATE_BELOW
-     * 2: windows not belonging in any other layer
-     * 3: windows of type _NET_WM_TYPE_DOCK (unless they have state _NET_WM_TYPE_BELOW) and
-     *    windows having state _NET_WM_STATE_ABOVE
-     * 4: focused windows having state _NET_WM_STATE_FULLSCREEN
-     */
-  if (w.content != null && w.content.id == "debugContent") // special case for the debug window, which sits above everything
-    return windowStack.layers[8];
-  if (w._net_wm_window_type == Atoms._NET_WM_WINDOW_TYPE_DESKTOP())
-    return windowStack.layers[0];
-  else if (w._net_wm_window_type == Atoms._NET_WM_WINDOW_TYPE_DOCK())
-    return windowStack.layers[3];
-  /* XXX we need cases 1 and 4 here */
-  else
-    return windowStack.layers[2];
-}
-
-
 windowStack.stackWindow = function (w) {
-  Debug ("w.className == " + w.className);
-  var l = windowStack.determineLayer (w);
+  var l = determineLayer (w);
   Debug ("adding window '" + w.content.id + "' to layer '" + l.id + "'");
   l.appendChild (w);
   w.layer = l;
-  windowStack.restackLayer (l);
+  restackLayer (l);
 }
 
 
@@ -82,26 +64,18 @@ windowStack.replaceWindow = function (w1, w2) {
 }
 
 
-windowStack.restackLayer = function (l) {
-  var z = 0;
-
-  for (var el = l.firstChild; el != null; el = el.nextSibling)
-    el.zIndex = z++;
-}
-
-
 windowStack.moveAbove = function(w, above) {
   if (above.layer == undefined)
-    windowStack.stackWindow (above, windowStack.determineLayer (above));
+    windowStack.stackWindow (above);
 
   if (w.layer == undefined)
-    windowStack.stackWindow (w, windowStack.determineLayer (w));
+    windowStack.stackWindow (w);
 
   if (above.layer != w.layer)
     return; // XXX
 
   above.layer.insertBefore (w, above.nextSibling);
-  windowStack.restackLayer (above.layer);
+  restackLayer (above.layer);
 }
 
 
@@ -123,7 +97,7 @@ windowStack.moveToBottom = function(w) {
   if (w.layer.firstChild != w)
     w.layer.insertBefore (w, w.layer.firstChild);
 
-  windowStack.restackLayer (l);
+  restackLayer (l);
 }
 
 
@@ -132,22 +106,92 @@ windowStack.moveToTop = function(w) {
     return;
 
   w.layer.insertBefore (w, null);
-  windowStack.restackLayer (w.layer);
+  restackLayer (w.layer);
 }
 
+
 windowStack.toggleDesktop = function () {
-  if (windowStack.showingDesktop) {
+  if (showingDesktop) {
     // hide everything above the desktop layer
-    for (var i = 1; i < windowStack.layers.length; i ++) {
-      windowStack.layers[i].style.display = "block";
+    for (var i = 1; i < layers.length; i ++) {
+      layers[i].style.display = "block";
     }
   }
   else {
     // hide everything above the desktop layer
-    for (var i = 1; i < windowStack.layers.length; i ++) {
-      windowStack.layers[i].style.display = "none";
+    for (var i = 1; i < layers.length; i ++) {
+      layers[i].style.display = "none";
     }
   }
 
-  windowStack.showingDesktop = !windowStack.showingDesktop;
+  showingDesktop = !windowStack.showingDesktop;
+}
+
+
+/// initialization
+
+var showingDesktop = false;
+var layers = new Array ();
+
+for (var el = windowStack.firstChild; el != null; el = el.nextSibling) {
+  if (el.id == "desktop-layer")         layers[0] = el;
+  else if (el.id == "below-layer")      layers[1] = el;
+  else if (el.id == "normal-layer")     layers[2] = el;
+  else if (el.id == "dock-layer")       layers[3] = el;
+  else if (el.id == "fullscreen-layer") layers[4] = el;
+  else if (el.id == "expose-layer")     layers[5] = el;
+  else if (el.id == "picker-layer")     layers[6] = el;
+  else if (el.id == "overlay-layer")    layers[7] = el;
+  else if (el.id == "debug-layer")      layers[8] = el;
+}
+
+// private methods
+
+// add a resize event handler on the parentNode so we can resize the
+// window stack in response to the document being layed out.
+windowStack.parentNode.onresize = function (e) {
+  updateSize ();
+}
+
+
+function updateSize () {
+  /* make sure the window stack takes up 100% of the size available to it */
+  windowStack.style.width = windowStack.parentNode.clientWidth;
+  windowStack.style.height = windowStack.parentNode.clientHeight;
+}
+
+
+function restackLayer (l) {
+  var z = 0;
+
+  for (var el = l.firstChild; el != null; el = el.nextSibling)
+    el.zIndex = z++;
+}
+
+
+function determineLayer (w) {
+  /* from EWMH:
+     from bottom to top:
+     * 0: windows of type _NET_WM_WINDOW_TYPE_DESKTOP
+     * 1: windows having state _NET_WM_STATE_BELOW
+     * 2: windows not belonging in any other layer
+     * 3: windows of type _NET_WM_TYPE_DOCK (unless they have state _NET_WM_TYPE_BELOW) and
+     *    windows having state _NET_WM_STATE_ABOVE
+     * 4: focused windows having state _NET_WM_STATE_FULLSCREEN
+     */
+  if (w.content != null && w.content.id == "debugContent") // special case for the debug window, which sits above everything
+    return layers[8];
+  if (w._net_wm_window_type == windowStack.parentNode.Atoms._NET_WM_WINDOW_TYPE_DESKTOP())
+    return layers[0];
+  else if (w._net_wm_window_type == windowStack.parentNode.Atoms._NET_WM_WINDOW_TYPE_DOCK())
+    return layers[3];
+  /* XXX we need cases 1 and 4 here */
+  else
+    return layers[2];
+}
+
+
+function Debug (str)
+{
+  windowStack.parentNode.Debug (str);
 }
