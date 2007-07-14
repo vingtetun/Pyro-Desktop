ed0455b6cc774da718aece95c6806a600dcfc143
toshok <toshok@bcc094ed-0e32-0410-8ff4-e4362d8942ad>
2007-07-14 08:13:04 +0000

    picker div work
    git-svn-id: http://pyrodesktop.org/svn/trunk/pyrodesktop@273 bcc094ed-0e32-0410-8ff4-e4362d8942ad

--- compzilla/compzilla/chrome/content/picker.js
+++ compzilla/compzilla/chrome/content/picker.js
@@ -1,110 +1,192 @@
+/* -*- mode: javascript; c-basic-offset: 4; indent-tabs-mode: t; -*- */
 
 var pickerLayer = $("#pickerLayer")[0];
 
-function newWindow (w)
-{
-  var win = $("#pickerItem").clone()[0];
-  win._contents = $("#window", win)[0];
-  win._label = $("#label", win)[0];
-
-  win._native = CompzillaWindowContent (w.content.nativeWindow);
-  win._contents.appendChild (win._native);
-
-  win._label.innerHTML = w.content.wmName;
-
-  win.style.display = "block";
-
-  return win;
-}
-
-var window_x = 20;
-
-function addWindow (el)
-{
-  Debug ("addWindow (" + el + ")");
-  $("#pickerWindow")[0].appendChild (el);
-  el.style.left = window_x;
-
-  window_x += 150 + 20;
-}
+var pickerWidth = 600;
+var pickerItemWidth = 150;
+var pickerItemPadding = 20;
 
 function renderBeauty ()
 {
-  var canvas = $("#pickerWindowBeauty")[0];
-  canvas.width = 620;
-  canvas.height = 200;
-  //  alert ("canvas = " + canvas.width + " x " + canvas.height);
-  var ctx = canvas.getContext('2d');
+    var canvas = $("#pickerWindowBeauty")[0];
+    canvas.width = pickerWidth;
+    canvas.height = 200;
+    var ctx = canvas.getContext('2d');
 
-  var gradient = ctx.createRadialGradient (canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2);
+    var gradient = ctx.createRadialGradient (canvas.width / 2, canvas.height / 2,
+					     0,
+					     canvas.width / 2, canvas.height / 2, canvas.width / 2);
 
-  gradient.addColorStop (0.0, 'rgba(0, 0, 0, 0.0)');
-  gradient.addColorStop (0.4, 'rgba(115, 155, 155, 0.4)');
-  gradient.addColorStop (0.7, 'rgba(180, 180, 180, 0.7)');
-  gradient.addColorStop (0.8, 'rgba(200, 200, 200, 0.8)');
-  gradient.addColorStop (1.0, 'rgba(255, 255, 255, 1.0)');
+    gradient.addColorStop (0.0, 'rgba(0, 0, 0, 0.0)');
+    gradient.addColorStop (0.4, 'rgba(115, 155, 155, 0.4)');
+    gradient.addColorStop (0.7, 'rgba(180, 180, 180, 0.7)');
+    gradient.addColorStop (0.8, 'rgba(200, 200, 200, 0.8)');
+    gradient.addColorStop (1.0, 'rgba(255, 255, 255, 1.0)');
 
-  ctx.fillStyle = gradient;
+    ctx.fillStyle = gradient;
 
-  ctx.fillRect (0, 0,
-		canvas.width, canvas.height);
+    ctx.fillRect (0, 0,
+		  canvas.width, canvas.height);
 }
 
-function populatePicker ()
-{
-  renderBeauty ();
 
-  for (var el = windowStack.firstChild; el != null; el = el.nextSibling) {
-    if (el.className == "uiDlg" /* it's a compzillaWindowFrame */
-	&& el.content.nativeWindow /* it has native content */
-	&& el.style.display == "block" /* it's displayed */) {
-      addWindow (newWindow (el));
+function newWindow (w)
+{
+    var item = $("#pickerItem").clone()[0];
+    item._contents = $("#window", item)[0];
+    item._label = $("#label", item)[0];
+
+    item._native = CompzillaWindowContent (w.content.nativeWindow);
+    item._contents.appendChild (item._native);
+
+    item._label.innerHTML = w.content.wmName;
+
+    item.style.display = "block";
+
+    return item;
+}
+
+function addWindow (el)
+{
+    $("#pickerContents")[0].appendChild (newWindow (el));
+}
+
+function populatePicker (forward)
+{
+    renderBeauty ();
+
+    for (var el = windowStack.firstChild; el != null; el = el.nextSibling) {
+	if (el.className == "uiDlg" /* it's a compzillaWindowFrame */
+	    && el.content.nativeWindow /* it has native content */
+	    && el.style.display == "block" /* it's displayed */) {
+	    addWindow (el);
+	}
     }
-  }
+
+    var vs = $(".pickerItem", $("#pickerContents"));
+
+    if (vs.length == 0) 
+	return;
+
+     var win_x;
+     if (forward) {
+ 	/* if we're moving foward, center the first window. */
+ 	win_x = vs[0].offsetLeft;
+ 	Debug (" x[0] = " + win_x);
+ 	for (var i = 1; i < vs.length; i ++) {
+ 	    win_x += pickerItemWidth + pickerItemPadding;
+ 	    var v = vs[i];
+ 	    v.style.left = win_x + "px";
+ 	    Debug (" x["+i+"] = " + win_x);
+ 	}
+     }
+     else {
+ 	/* if we're moving backward, center the last window. */
+ 	win_x = vs[vs.length - 1].offsetLeft;
+ 	for (var i = vs.length - 2; i >= 0; i --) {
+ 	    win_x -= pickerItemWidth + pickerItemPadding;	
+ 	    var v = vs[i];
+ 	    v.style.left = win_x + "px";
+ 	}
+     }
+}
+
+function clearPicker ()
+{
+    // this doesn't work?  $("#pickerWindow").remove(".pickerItem");
+
+    var vs = $(".pickerItem", $("#pickerContents"));
+    for (i = 0; i < vs.length; i ++) {
+	var v = vs[i];
+	$("#pickerContents")[0].removeChild (v);
+    }
 }
 
 var shown = false;
 
 function showPicker (forward)
 {
-  Debug ("showPicker called");
-  Debug (pickerLayer);
-  Debug (pickerLayer.style.display);
+    if (!shown) {
+      	populatePicker (forward);
 
-  if (!shown) {
-    Debug ("yay!");
-      	populatePicker ();
+	$("#pickerContents")[0].style.left = 200 + "px";
 
   	pickerLayer.style.display = "block";
-	// XXX install key release handler to hide the layer
+
+	document.addEventListener("keyup", {
+	                              handleEvent: function (event) {
+					  if (event.keyCode == event.DOM_VK_CONTROL) {
+					      pickerLayer.style.display = "none";
+					      shown = false;
+
+					      clearPicker ();
+
+					      document.removeEventListener ("keyup", this, true);
+					  }
+				      }
+				  },
+				  true);
 
 	shown = true;
-  }
-  else
-    Debug ("BOO");
 
-   if (forward)
-     nextWindow ();
-   else
-     prevWindow ();
+
+        if (forward) {
+	    var vs = $(".pickerItem", $("#pickerContents"));
+
+	    selected_window = 0;
+
+	    //	    $(vs[selected_window]).addClass ("selectedPickerItem");
+        }
+        else {
+	    var vs = $(".pickerItem", $("#pickerContents"));
+
+	    selected_window = vs.length - 1;
+
+	    //	    $(vs[selected_window]).addClass ("selectedPickerItem");
+        }
+    }
+
+    var vs = $(".pickerItem", $("#pickerContents"));
+    if (vs.length <= 1) {
+	return;
+    }
+
+    if (forward) {
+	nextWindow ();
+    }
+    else {
+	prevWindow ();
+    }
 }
+
+var selected_window;
 
 function nextWindow ()
 {
-  var vs = $(".pickerItem", $("#pickerWindow"));
+    var v = $("#pickerContents")[0];
 
-  for (i = 0; i < vs.length; i ++) {
-    var v = vs[i];
+    Debug ("moving div to " + (v.offsetLeft - 220));
+
+    //    $(vs[selected_window]).toggleClass ("selectedPickerItem");
+
+    selected_window ++;
+
+    //    $(vs[selected_window]).toggleClass ("selectedPickerItem");
+
     $(v).animate ({left: v.offsetLeft - 220 }, "linear");
-  }
 }
 
 function prevWindow ()
 {
-  var vs = $(".pickerItem", $("#pickerWindow"));
+    var v = $("#pickerContents")[0];
 
-  for (i = 0; i < vs.length; i ++) {
-    var v = vs[i];
+    Debug ("moving div to " + (v.offsetLeft + 220));
+
+    //    $(vs[selected_window]).toggleClass ("selectedPickerItem");
+
+    selected_window --;
+
+    //    $(vs[selected_window]).toggleClass ("selectedPickerItem");
+
     $(v).animate ({left: v.offsetLeft + 220 }, "linear");
-  }
 }
